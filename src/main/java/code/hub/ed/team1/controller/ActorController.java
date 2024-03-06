f15ee30349961f12ada1067b5aed333c7ea3e48b
kostas.chatzifotiadis <k.chatzifotiadis@gmail.com>
2024-03-06 11:00:15 +0200

    Actor/Movie controller

--- src/main/java/code/hub/ed/team1/controller/ActorController.java
+++ src/main/java/code/hub/ed/team1/controller/ActorController.java
@@ -2,11 +2,15 @@ package code.hub.ed.team1.controller;
 
 import code.hub.ed.team1.dto.ActorDto;
 import code.hub.ed.team1.dto.PeopleDto;
-import code.hub.ed.team1.service.api.PeopleService;
+import code.hub.ed.team1.service.api.ActorService;
 import lombok.AllArgsConstructor;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,11 +20,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class ActorController {
 
 
-  private PeopleService actorService;
+  private ActorService actorService;
 
   @GetMapping("/{id}")
   public PeopleDto getMovieById(@PathVariable("id") long id) {
     return actorService.read(id);
   }
 
+  @PostMapping("/create")
+  public ActorDto createActor(@RequestBody ActorDto actorDto) {
+    return actorService.create(actorDto);
+  }
+
+  @PutMapping("/update")
+  public ActorDto updateActor(@RequestBody ActorDto actorDto) {
+    return actorService.update(actorDto);
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteActor(@PathVariable long id) {
+    actorService.delete(id);
+  }
 }
--- src/main/java/code/hub/ed/team1/controller/MovieController.java
+++ src/main/java/code/hub/ed/team1/controller/MovieController.java
@@ -4,8 +4,12 @@ import code.hub.ed.team1.dto.MovieDto;
 import code.hub.ed.team1.service.api.MovieService;
 import jakarta.websocket.server.PathParam;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +18,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/movie")
 public class MovieController {
 
-    private final MovieService movieService;
+  private final MovieService movieService;
 
-    @GetMapping("/{id}")
-    public MovieDto getMovieById(@PathVariable("id") long id) {
-        return movieService.read(id);
-    }
+  @GetMapping("/{id}")
+  public MovieDto getMovieById(@PathVariable("id") long id) {
+    return movieService.read(id);
+  }
+
+  @PostMapping("/create")
+  public MovieDto createMovie(@RequestBody MovieDto movieDto) {
+    return movieService.create(movieDto);
+  }
+
+  @PutMapping("/update")
+  public MovieDto updateMovie(@RequestBody MovieDto movieDto) {
+    return movieService.update(movieDto);
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteMovie(@PathVariable long id) {
+    movieService.delete(id);
+  }
 }
